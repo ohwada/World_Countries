69988dbf9272968727ba17dae2fdabd594faddf8
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-06-28 22:12:20 +0900

    add crown_dependencies

---
+++ national_flags_gmap/crown_dependencies/README.md
@@ -0,0 +1,22 @@
+Crown Dependencies
+===============
+
+List of Crown Dependencies
+
+ The Crown Dependencies are three flag territories in the British Islands that are self-governing possessions of the British Crown
+
+create KML file from JSON file
+
+import KML file to Mymap
+
+[mymaps: Crown Dependencies](https://www.google.com/maps/d/viewer?mid=1ZVuJVFlWmUGb6ozsHu4cSKrixTyDBws&ll=51.435898456958114%2C-3.29600000000001&z=4
+
+display the Flag at the Coordinates of Islands on Google Map
+
+![crown dependencies](https://github.com/ohwada/World_Countries/blob/main/national_flags_gmap/un_member_states/screenshots/un_members_capital.png)
+
+#### Python program
+- json2kml.py
+
+#### Reference
+- https://github.com/ohwada/World_Countries/tree/main/crown_dependencies_coordinates
---
+++ national_flags_gmap/crown_dependencies/python/json2kml.py
@@ -0,0 +1,68 @@
+# json2kml.py
+# 2023-06-01 K.OHWADA
+
+import json
+
+import urllib.parse
+
+TEMPLATE_A_TAG = '<a href="{href}">{name}</a>'
+
+TEMPLATE_IMG = '<img src="{src}" decoding="async"  width="{width}" height="{height}" />'
+
+TEMPLATE_DESCRIPTION = '<![CDATA[<a href="{url_country}">{country}</a>]]>'
+
+TEMPLATE_STYLE_URL = '#{id}'
+
+TITLE = 'Crown Dependencies'
+
+DESC = 'display the Flag at the Coordinates of Islands'
+
+SPACE = ' '
+
+UNDERBAR = '_'
+
+ALTITUDE = 0
+
+with open('template_kml.txt', 'r') as f1:
+    template_kml = f1.read()
+
+with open('template_placemark.txt', 'r') as f2:
+    template_placemark = f2.read()
+
+with open('template_style.txt', 'r') as f3:
+    template_style = f3.read()
+
+teritories =[]
+
+placemarks = ""
+
+styles = ""
+
+with open('crown_dependencies_coordinates.json') as f3:
+    dic = json.load(f3)
+    str_title = dic['title']
+    list_teritories = dic['teritories']
+    
+    for item in list_teritories:
+        island = item['island']
+        url_island = item['url_island']
+        lat = item['lat']
+        lon = item['lon']
+        url_flag = item['url_flag']
+        url_flag_icon = item['url_flag_icon']
+        print(    island)
+        desc = TEMPLATE_DESCRIPTION.format(url_country=url_island, country=island)
+        id_island = urllib.parse.quote( island.lower().replace(SPACE, UNDERBAR) )
+        style_url = TEMPLATE_STYLE_URL.format(id=id_island)
+        placemark = template_placemark.format(name=island, description=desc,  lat=lat,  lon=lon, altitude=ALTITUDE,  style_url =  style_url)
+        print( placemark)
+        style = template_style.format(id=id_island, href=url_flag_icon)
+
+        placemarks += placemark
+        styles += style
+
+wdata = template_kml.format(name=TITLE, description=DESC, placemarks= placemarks, styles=styles)
+  
+with open('crown_dependencies.kml', 'w') as f4:
+    f4.write(wdata)
+
