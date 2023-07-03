4e745f76c750848cc7ceef037f2eadeae038ee8d
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-07-03 23:19:36 +0900

    update

--- countries_independent_from_uk_capital/README.md
+++ countries_independent_from_uk_capital/README.md
@@ -1,19 +1,20 @@
-Countries ndependent from the UK Capital
+UK Colonies
 ===============
 
-List of countries that have gained independence from the United Kingdom with Capital
+List of countries that have gained independence from the United Kingdom
 
 Below are lists of the countries and territories formerly ruled or administered by the United Kingdom or part of the British Empire (including military occupations that did not retain the pre-war central government), with their independence days. 
 A total of 65 countries have claimed their independence from the British Empire or the United Kingdom.
 
-![countries ndependent from the uk capital](https://github.com/ohwada/World_Countries/blob/main/countries_independent_from_uk_capital/screenshots/countries_from_uk_capital.png)
+![uk_colonies](https://github.com/ohwada/World_Countries/blob/main/uk_colonies/screenshots/uk_colonies.png)
 
 - List of Member states in JSON file
 - Python program which create List in HTML form
 
 The following Web page has List of Member states in HTML tabular form.
 
-[wikipedia: List of countries that have gained independence from the United Kingdom](https://en.wikipedia.org/wiki/List_of_countries_that_have_gained_independence_from_the_United_Kingdom)
+wikipedia: List of countries that have gained independence from the United Kingdom
+https://en.wikipedia.org/wiki/List_of_countries_that_have_gained_independence_from_the_United_Kingdom
 
 Extract the information from Web page
 and make it into a JSON format file.
@@ -24,5 +25,6 @@ Create List in HTML form from JSON file
 - sv2json.py
 - json2html.py
 
-## Refernce
+### Reference
 https://ken-ohwada.hatenadiary.org/entry/2023/07/02/194219
+
--- countries_independent_from_uk_capital/python/merge_json.py
+++ countries_independent_from_uk_capital/python/merge_json.py
@@ -40,32 +40,60 @@ wdata = ""
 
 with open('countries_from_uk.json') as f1:
     dic1 = json.load(f1)
-    list_uk_colonies = dic1['countries']
+    list_countries = dic1['countries']
 
 with open('un_members_capital.json') as f2:
     dic2 = json.load(f2)
     list_un_members = dic2['countries']
 
-for item in  list_uk_colonies:
+with open('countries_from_uk_dominion.json') as f3:
+    dic3 = json.load(f3)
+    list_dominion = dic3['countries']
+
+for item in  list_countries:
     country = item['country']
     print(country)
     url_country = item['url_country']
+    url_flag_icon = item['url_flag_icon']
+    icon_width = item['icon_width']
+    icon_height = item['icon_height']
 
     is_match, matched = find_country(list_un_members, country, url_country)
  
     capital = '-'
     url_capital = '-'
-    url_flag_icon = '-'
-    icon_width = 0
-    icon_height = 0
     lat = 0
     lon = 0
     if is_match:
         capital = replace_comma_html( matched['capital'] )
         url_capital =  matched['url_capital']
-        url_flag_icon = matched['url_flag_icon']
-        icon_width = matched['icon_width']
-        icon_height = matched['icon_height']
+        lat = matched['lat']
+        lon = matched['lon']
+
+    line = FORMAT_LINE.format(country=country, url_country=url_country, capital=capital, url_capital=url_capital, url_flag_icon= url_flag_icon,   icon_width=  icon_width, icon_height=icon_height, lat=lat, lon=lon)
+    print(line)
+    wdata += line;
+#
+
+
+
+for item in  list_dominion:
+    country = item['country']
+    print(country)
+    url_country = item['url_country']
+    url_flag_icon = item['url_flag_icon']
+    icon_width = item['icon_width']
+    icon_height = item['icon_height']
+
+    is_match, matched = find_country(list_un_members, country, url_country)
+ 
+    capital = '-'
+    url_capital = '-'
+    lat = 0
+    lon = 0
+    if is_match:
+        capital = replace_comma_html( matched['capital'] )
+        url_capital =  matched['url_capital']
         lat = matched['lat']
         lon = matched['lon']
 
