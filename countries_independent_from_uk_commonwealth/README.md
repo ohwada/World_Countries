ee4fda7979734d5fbc565a7f9b8a771f0e7009c5
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-07-03 23:15:12 +0900

    add countries_independent_from_uk_commonwealth

---
+++ countries_independent_from_uk_commonwealth/README.md
@@ -0,0 +1,29 @@
+Countries independent from UK commonwealth
+===============
+
+List of countries that have gained independence from the United Kingdom with 
+member of Commonwealth nations
+
+Below are lists of the countries and territories formerly ruled or administered by the United Kingdom or part of the British Empire (including military occupations that did not retain the pre-war central government), with their independence days. 
+A total of 65 countries have claimed their independence from the British Empire or the United Kingdom.
+
+![countries independent from uk  commonwealth]()
+
+- List of Member states in JSON file
+- Python program which create List in HTML form
+
+The following Web page has List of Member states in HTML tabular form.
+
+wikipedia: List of countries that have gained independence from the United Kingdom
+https://en.wikipedia.org/wiki/List_of_countries_that_have_gained_independence_from_the_United_Kingdom
+
+merger JSON files
+- countries_from_uk.json countries_from_uk
+- un_members_flag.json [un_members](https://github.com/ohwada/World_Countries/tree/main/un_member_states)
+- commonwealth_nations.json [commonwealth_nations](https://github.com/ohwada/World_Countries/tree/main/commonwealth_nations)
+
+#### Python program
+- merge_json.py
+- sv2json.py
+- json2html.py
+
---
+++ countries_independent_from_uk_commonwealth/python/json2html.py
@@ -0,0 +1,62 @@
+# json2html.py
+# 2023-06-01 K.OHWADA
+
+import json
+
+def conv_yn(data):
+    yn= 'Yes' if data ==1 else 'No'
+    return yn
+#
+
+TEMPLATE_A_TAG = '<a href="{href}">{name}</a>'
+
+TEMPLATE_IMG = '<img src="{src}" decoding="async"  width="{width}" height="{height}" />'
+
+WIDTH = 100
+
+with open('template_html.txt', 'r') as f1:
+    template_html = f1.read()
+
+with open('template_row.txt', 'r') as f2:
+    template_row = f2.read()
+
+countries =[]
+
+rows=""
+
+with open('countries_from_uk_commonwealth.json') as f3:
+    dic = json.load(f3)
+    str_title = dic['title']
+    table_title = dic['table_title']
+
+    desc = dic['desc']
+    ref = dic['reference']
+    url_ref = dic['url_reference']
+    list_countries = dic['countries']
+    print(str_title)
+    
+    for item in list_countries:
+        country = item['country']
+        url_country = item['url_country']
+        url_flag_icon = item['url_flag_icon']
+        icon_width = item['icon_width']
+        icon_height = item['icon_height']
+        year = item['year']
+        un_member = conv_yn( item['un_member'] )
+        commonwealth_nations = conv_yn( item['commonwealth_nations'] )
+        notes = item['notes']
+
+        row_country = TEMPLATE_A_TAG.format(href=url_country, name=country)
+        row_flag_icon = TEMPLATE_IMG.format(src=url_flag_icon, width=icon_width, height=icon_height)
+        row = template_row.format(country=row_country, flag_icon= row_flag_icon, year=year, un_member =un_member ,  commonwealth_nations= commonwealth_nations,  notes=notes)
+        print(row)
+        rows +=  row
+#
+
+html_ref = TEMPLATE_A_TAG.format(href=url_ref, name=ref)
+
+wdata = template_html.format(body_title=str_title, table_title=table_title, desc = desc, reference=html_ref, rows=rows)
+  
+with open('countries_from_uk_commonwealth.html', 'w') as f4:
+    f4.write(wdata)
+
