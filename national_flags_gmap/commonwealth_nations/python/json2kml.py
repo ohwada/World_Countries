ef191b3f5fb6e570f0dc49fb2e75a5108a555e52
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-07-20 15:43:02 +0900

    update

--- national_flags_gmap/commonwealth_nations/python/json2kml.py
+++ national_flags_gmap/commonwealth_nations/python/json2kml.py
@@ -1,69 +1,42 @@
 # json2kml.py
 # 2023-06-01 K.OHWADA
 
+import simplekml
 import json
-
 import urllib.parse
 
-TEMPLATE_A_TAG = '<a href="{href}">{name}</a>'
+FORMAT_DESCRIPTION = '<![CDATA[<a href="{href}">{name}</a>]]>'
 
-TEMPLATE_IMG = '<img src="{src}" decoding="async"  width="{width}" height="{height}" />'
+# Document
+DOC_NAME = 'Commonwealth Nations'
+DOC_DESC = 'display the National Flag at the Coordinates of the Capital'
+FILENAME = 'commonwealth_nations.kml'
 
-TEMPLATE_DESCRIPTION = '<![CDATA[<a href="{url_country}">{country}</a>]]>'
+# create kml
+kml = simplekml.Kml()
+kml.document.name = DOC_NAME
+kml.document.description = DOC_DESC
 
-TEMPLATE_STYLE_URL = '#{id}'
-
-TITLE = 'Commonwealth Nations'
-
-DESC = 'display the National Flag at the Coordinates of the Capital'
-
-SPACE = ' '
-
-UNDERBAR = '_'
-
-ALTITUDE = 0
-
-with open('template_kml.txt', 'r') as f1:
-    template_kml = f1.read()
-
-with open('template_placemark.txt', 'r') as f2:
-    template_placemark = f2.read()
-
-with open('template_style.txt', 'r') as f3:
-    template_style = f3.read()
-
-countries =[]
-
-placemarks = ""
-
-styles = ""
-
-with open('commonwealth_nations_capital.json') as f3:
-    dic = json.load(f3)
-    str_title = dic['title']
+with open('commonwealth_nations_capital.json') as f1:
+    dic = json.load(f1)
     list_countries = dic['countries']
-    
-    for item in list_countries:
-        country = item['country']
-        url_country = item['url_country']
-        capital = item['capital']
-        url_capital = item['url_capital']
-        lat = item['lat']
-        lon = item['lon']
-        url_flag_icon = item['url_flag_icon']
-        print(country)
-        desc = TEMPLATE_DESCRIPTION.format(url_country=url_country, country=country)
-        id_country = urllib.parse.quote( country.lower().replace(SPACE, UNDERBAR) )
-        style_url = TEMPLATE_STYLE_URL.format(id=id_country)
-        placemark = template_placemark.format(name=country, description=desc,  lat=lat,  lon=lon, altitude=ALTITUDE,  style_url =  style_url)
-        print( placemark)
-        style = template_style.format(id=id_country, href=url_flag_icon)
+#
 
-        placemarks += placemark
-        styles += style
 
-wdata = template_kml.format(name=TITLE, description=DESC, placemarks= placemarks, styles=styles)
-  
-with open('ommonwealth_nations_capital..kml', 'w') as f4:
-    f4.write(wdata)
+# Placemark
+for item in list_countries:
+	country = item['country']
+	url_country = item['url_country']
+	url_flag_icon = item['url_flag_icon']
+	lat = item['lat']
+	lon = item['lon']
+	print(country)
+
+	point = kml.newpoint(name = country)
+	point.coords = [(lon, lat)]
+	point.style.iconstyle.icon.href = url_flag_icon
+	point.description =  FORMAT_DESCRIPTION.format(href=url_country, name=country)
+#
+
+kml.save(FILENAME)
 
