14e8521c1852c1b83ec868fd30a47b0d04617a99
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-07-26 06:48:23 +0900

    add japan_prefectures

---
+++ folium/japan_prefectures/README.md
@@ -0,0 +1,14 @@
+Japan Prefectures
+===============
+
+display Prefectures in Japan
+
+[japan prefectures]()
+
+
+[folium: GeoJson](https://python-visualization.github.io/folium/modules.html#folium.features.GeoJson)  
+Creates a GeoJson object for plotting into a Map.  
+
+[folium: GeoJsonopup](https://python-visualization.github.io/folium/modules.html#folium.features.GeoJsonPopup)
+
+[47都道府県のポリゴンデータ](https://japonyol.net/editor/article/47-prefectures-geojson.html)
---
+++ folium/japan_prefectures/python/japan_prefectures.py
@@ -0,0 +1,58 @@
+# japan_prefectures.py
+# 2023-06-01 K.OHWAA
+
+import folium
+import json
+import random
+
+
+TITLE = 'Japan Prefectures'
+
+FILEPATH = 'japan_prefectures.html'
+
+FORMAT_TITLE = '<h3 align="center">{}</h3>'
+             
+name = 'prefectures'
+url_geojson = 'https://raw.githubusercontent.com/ohwada/World_Countries/master/leaflet/japan_prefectures/data/prefectures.geojson'
+
+# Tokyo Japan
+name = 'Tokyo Japan'
+lat = 35.689722
+lon = 139.692222
+ZOOM = 4
+
+
+with open('web_colors_100.json') as f1:
+    dic = json.load(f1)
+#
+
+list_colors = dic['colors']
+
+
+def style_function(feature):
+	len_list = len( list_colors)
+	rint = random.randint(0, (len_list -1) )
+	item = list_colors[rint]
+	color = item['hex']
+	return {
+	"fillOpacity": 0.5,
+	"weight": 2,
+	"color": "black",
+	"fillColor": color
+    }
+#
+
+
+# Map
+map = folium.Map(location=[lat, lon], tiles="cartodbpositron", zoom_start=ZOOM)
+
+gjson = folium.GeoJson(url_geojson, name=name, style_function=style_function).add_to(map)
+
+folium.features.GeoJsonPopup( fields=['name'], labels=False ).add_to(gjson)
+
+# add title
+title_html = FORMAT_TITLE.format(TITLE)
+map.get_root().html.add_child(folium.Element(title_html))
+
+map.save(FILEPATH )
+
