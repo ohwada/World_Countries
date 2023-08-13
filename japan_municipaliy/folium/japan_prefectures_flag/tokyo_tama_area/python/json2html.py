c5caa8311878252d013424cf8d12e1ae24e25d3c
Kenichi Ohwada <ml.ohwada@gmail.com>
2023-08-13 12:39:31 +0900

    add tokyo_tama_area

---
+++ japan_municipaliy/folium/japan_prefectures_flag/tokyo_tama_area/python/json2html.py
@@ -0,0 +1,87 @@
+# json2html.py
+# 2023-06-01 K.OHWADA
+
+import json
+
+
+FILE_JSON = 'tokyo_tama_area_list.json'
+
+FILE_HTML = 'tokyo_tama_area_list.html'
+
+FORMAT_A_TAG = '<a href="{href}" target="_blank">{name}</a>'
+
+FORMAT_IMG = '<img src="{src}" decoding="async"  width="{width}" height="{height}" />'
+
+FORMAT_GMAP ='https://www.google.com/maps/@{lat},{lon},{zoom}z'
+
+FORMAT_LATLON = '({lat:.1f}, {lon:.1f})'
+
+
+ZOOM = 12
+
+CITY_WIDTH = 200
+
+
+def make_coordinates(lat, lon):
+    gmap = FORMAT_GMAP.format(lat=lat, lon=lon, zoom=ZOOM)
+    latlon =FORMAT_LATLON.format( lat=lat, lon=lon )
+    atag =FORMAT_A_TAG.format(href=gmap, name=latlon)
+    return atag
+#
+
+
+with open('files/template_html.txt', 'r') as f1:
+    template_html = f1.read()
+#
+
+with open('files/template_row.txt', 'r') as f2:
+    template_row = f2.read()
+#
+
+rows = ''
+
+with open(FILE_JSON, 'r') as f3:
+    dic = json.load(f3)
+#
+
+title_ja = dic['title_ja']
+ref = dic['reference']
+url_ref = dic['url_reference']
+list_cities = dic['cities']
+
+
+for item in list_cities :
+    number = item['number']
+    code = item['code']
+    name_ja = item['name_ja']
+    name_en = item['name_en']
+    url_city = item['url_city']
+    url_flag = item['url_flag']
+    flag_width = item['flag_width']
+    flag_height = item['flag_height']
+    population = item['population']
+    area = item['area']
+    density = item['population_density']
+    revenue = item['revenue']
+    established = item['established']
+    name_en = item['name_en']
+    lat = item['lat']
+    lon = item['lon']
+    row_city = FORMAT_A_TAG.format(href=url_city, name=name_ja)
+    row_flag = FORMAT_IMG.format(src=url_flag, width=flag_width, height=flag_height)
+    row_coordinates = make_coordinates(lat, lon)
+    row = template_row.format(number = number,  code=code, city=row_city, flag=row_flag, population=population, area =area, density=density, revenue=revenue, established=established, name_en=name_en, coordinates= row_coordinates, city_width= CITY_WIDTH )
+    print(row)
+    rows +=  row
+#
+
+
+html_ref = FORMAT_A_TAG.format(href=url_ref, name=ref)
+
+wdata = template_html.format(body_title=title_ja, reference=html_ref, rows=rows)
+
+
+with open(FILE_HTML, 'wt') as f4:
+    f4.write(wdata)
+#
+
---
+++ japan_municipaliy/tokyo_tama_area/README.md
@@ -0,0 +1,13 @@
+Tokyo Tama area
+===============
+
+東京多摩地域の市町村の一覧
+
+List of cities in Tokyo Tama area
+
+![tokyo tama area]()
+
+### Reference
+
+[wikipedia: 多摩地域](https://ja.wikipedia.org/wiki/%E5%A4%9A%E6%91%A9%E5%9C%B0%E5%9F%9F)
+
